619480f229cd6116c7d8be4992c80c94e7d4a44e
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-06-02 09:35:10 +0200

    Display homepage by js

---
+++ src/home.js
@@ -0,0 +1,85 @@
+
+
+
+const content = document.querySelector('.contentJs');
+
+// headline elements
+const headline = document.createElement('div');
+headline.classList.add('headline');
+const leftHead = document.createElement('div');
+leftHead.classList.add('left-head');
+leftHead.innerHTML =  `Pizza delucia
+                <p>
+                    The most tasty <span id="pizza">pizza</span> in the World!
+                </p>`;
+const rightHead = document.createElement('div');
+rightHead.classList.add('right-head');
+headline.appendChild(leftHead);
+headline.appendChild(rightHead);
+content.appendChild(headline);
+ // headline done
+ // content container -- first section Text about restaurant
+ const cntContainer = document.createElement('div');
+ cntContainer.classList.add('container-content');
+ const text = document.createElement('div');
+ text.classList.add('text');
+ text.innerText = 'Our restaurant is a reflection of the pleasure of the best taste. We have been making our pizza for years and our recipes come from traditional methods, selectively selected over the years so that every bite is an unforgettable delicacy!';
+ cntContainer.appendChild(text);
+ content.appendChild(cntContainer);
+ // content container Hours
+ const infoC = document.createElement('div');
+ infoC.classList.add('info-container');
+ const openC = document.createElement('div');
+ openC.id = 'open-times'
+ const openH = document.createElement('div');
+ openH.classList.add('open-hours', 'flex-rows');
+ openH.innerText = 'Open Hours';
+ openC.appendChild(openH)
+ infoC.appendChild(openC)
+ const location = document.createElement('div');
+ location.id = 'location';
+ infoC.appendChild(location)
+ cntContainer.appendChild(infoC);
+
+ const weeksArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+ const timesArray = ['10:00 AM - 22:00 PM', '10:00 AM - 2:00 AM']
+ function createRows(){
+    for(let i = 0; i < weeksArray.length; i++){
+        const flexRows = document.createElement('div');
+        const week = document.createElement('div');
+        const times = document.createElement('div');
+        flexRows.className = 'flex-rows';
+        week.textContent = weeksArray[i];
+        if(i < 5){
+            times.textContent = timesArray[0]
+        } else{
+            times.textContent = timesArray[1];
+        }
+        flexRows.appendChild(week);
+        flexRows.appendChild(times);
+        openC.appendChild(flexRows)
+
+    }
+ }
+ const locationArray = ['England', 'Poland', 'India'];
+ const streets = ['London, street: Dreams 21/a', 'Warsaw, street: Babowicza 11', 'New Dehli, street: Abbratuli 7'];
+
+ function createTimes(){
+    const openH2 = document.createElement('div');
+    openH2.classList.add('open-hours', 'flex-rows');
+    openH2.textContent = 'Locations';
+    location.appendChild(openH2);
+    for(let i = 0; i < locationArray.length; i++){
+        const flexRows2 = document.createElement('div');
+        flexRows2.className = 'flex-rows2'
+        const loc = document.createElement('div');
+        const street = document.createElement('div');
+        loc.textContent = locationArray[i];
+        street.textContent = streets[i]
+        flexRows2.appendChild(loc);
+        flexRows2.appendChild(street);
+        location.appendChild(flexRows2)
+    }
+ }
+ createRows()
+ createTimes()
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './styles/style.css'
 import './styles/nav-styles.css'
-
+import './home.js'
 console.log('testing');
